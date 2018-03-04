77fb8b72b907c6cc642d0af23c8fc5da628de162
Rootshell <bcairns@voidrealms.com>
2018-03-04 15:20:10 -0500

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:poly1/feline.dart';
+
+main(List<String> arguments) {
+
+
+  Feline cat = new Feline();
+
+  cat.breath();
+
+  cat.test();
+
+}
---
+++ test/poly1_test.dart
@@ -0,0 +1,8 @@
+import 'package:poly1/poly1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
